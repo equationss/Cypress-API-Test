f58421f46f51ba0d520c457df80a91fc2cf5049b
Raja Junaid Ullah Khan <junaidraja2@gmail.com>
2023-07-24 11:07:46 +0500

    New Update

--- cypress/e2e/APi Test.cy.js
+++ cypress/e2e/APi Test.cy.js
@@ -1,51 +1,51 @@
 describe('API Test', () => {
 
-  //This block inserts the data which is mentioned in the body of the request
+  //This request add data which is mentioned in the body of the request
   it('POST request', () => {
   
   cy.request({method: 'POST', url: 'https://reqres.in/api/users', body: {
-  "name": "Bingo",
+  "name": "Junaid",
   "job": "Team lead"
   }}).then((response) =>{
     
   //Asserting the status code to be 201 for successful execution
   expect(response.status).to.eq(201)
   //Asserting the name which we have inserted into
-  expect(response.body.name).to.eq("Bingo")
-  //Asserting the status text to confirm whether it is created
+  expect(response.body.name).to.eq("Junaid")
+  //Asserting the status to confirm whether it is created
   expect(response.statusText).to.eq("Created")
   })
   })
   
-  //This block will execute PUT request
+  //This request will execute PUT request
   it('PUT request', () => {
   
   cy.request({method: 'PUT', url: 'https://reqres.in/api/users/2', body: {
-  "name": "Angel",
-  "job": "zion resident"
+  "name": "ALi",
+  "job": "Resident"
   }}).then((response) =>{
   //Asserting the status code to be 200 for successful execution
   expect(response.status).to.eq(200)
   //Asserting the name which we have inserted into
-  expect(response.body.name).to.eq("Angel")
+  expect(response.body.name).to.eq("Ali")
   })
   })
   
-  //This block will execute the PATCH request and update the record
+  //This request will execute the PATCH request and update the record
   it('PATCH request', () => {
   
   cy.request({method: 'PATCH', url: 'https://reqres.in/api/users/2', body: {
-  "name": "Angel",
-  "job": "zion resident"
+  "name": "Ali",
+  "job": "Resident"
   }}).then((response) =>{
   //Asserting the status code to be 200 for successful execution
   expect(response.status).to.eq(200)
   //Asserting the name which we have inserted into
-  expect(response.body.name).to.eq("Angel")
+  expect(response.body.name).to.eq("Ali")
   })
   })
   
-  //This block will delete the user which exist
+  //This request will delete the user which exist
   it('DELETE request', () => {
   
   cy.request({method: 'DELETE', url: 'https://reqres.in/api/users/2'}).then((response) =>{
